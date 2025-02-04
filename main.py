298936b5f5ad489ef475ae45655ec3f99a820aa4
ninesl <lance.nines@gmail.com>
2025-02-04 16:52:20 -0500

    modified predict script to have Fetcher class, encapsulates DB queries

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+'''
+Script responsible for HTTP requests to the ML pedigree microservice
+usage: `fastapi {dev}/{run} repos/py-pedigree-connect/main.py`
+remeber to check host:8000/docs and /redoc
+'''
+
+import os
+import psycopg2
 from fastapi import FastAPI
-from typing import Union
+# from typing import Union
 from pydantic import BaseModel
 from dotenv import load_dotenv
+from predict import PedigreePredictor, PedigreeDataLoader, PedigreeFetcher
 
-MASTER_CONN = connect_db()
 
 def connect_db():
+    '''connect to the db based on .env'''
     load_dotenv()
     conn = psycopg2.connect(
         host=os.getenv('PSQL_HOST'),
@@ -16,16 +25,37 @@ def connect_db():
     )
     return conn
 
+
+fetcher = PedigreeFetcher(conn=connect_db())
+loader = PedigreeDataLoader(fetcher=fetcher)
+predictor = PedigreePredictor()
 app = FastAPI()
 
+
 class Input(BaseModel):
+    '''auto serialzes into JSON automagically. input is for predictions'''
     sire_name: str
     sire_yob: int
     dam_name: str
     dam_yob: int
     sex: str
     year_born: int
-    
+
+
 @app.post("/predict")
-def predict(input: Input):
-    return {"result": input}
+def predict(data: Input):
+    '''pass in input, get giant JSON blob back'''
+    global fetcher
+    # input_data = loader.build_input_dict(data) #explicity declare this.
+
+    for sex in ['Male', 'Female', 'Gelding']:
+        data.sex = sex
+        input_data = loader.build_input_dict(data)
+        predictions = predictor.predict(input_data)
+
+        print(f"\n\033[1m{sex.upper()}\nby {input_args.sire_name}, out of {input_args.dam_name}")
+        print_predictions(predictions)
+        print("-" * 80)
+    fetcher.print_pedigree_details(input_data)
+    print("\n" * 10)
+    return {"result": data}
